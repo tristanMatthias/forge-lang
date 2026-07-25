7e0a54b0c2e052001672a7d1288fdaed4d34b5ea
Tristan Mathias <github.gqmij@simplelogin.com>
2026-07-24 22:35:44 -0700

    test(grammar): gate native paren recovery parity

--- bootstrap/llvm_wrapper.c
+++ bootstrap/llvm_wrapper.c
@@ -10,6 +10,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include <unistd.h>
 
 // All functions use the avra_llvm_* naming convention matching
 // the extern fn declarations in src/core/llvm.fg.
